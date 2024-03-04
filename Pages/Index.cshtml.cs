a8989c74bd81acefe756fbb089dcc2cd7e5af79e
HaiPKH <100670221+HaiPKH@users.noreply.github.com>
2024-03-04 14:02:25 +0700

    Finished Video Downloading function

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,69 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Specialized;
+using System.Net;
+using System.IO;
+using YoutubeExplode;
+
 
 namespace PRN211FinalProj.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public string Url { get; set; }
 
+        [BindProperty]
+        public string Format { get; set; }
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnPost()
         {
+            try
+            {
+                Console.WriteLine("Begin Download");
+                await DownloadYouTubeVideo(Url, Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while downloading the videos: " + ex.Message);
+            }
+            return Page();
+        }
 
+        public async Task DownloadYouTubeVideo(string videoUrl, string outputDirectory)
+        {
+            var youtube = new YoutubeClient();
+            var video = await youtube.Videos.GetAsync(videoUrl);
+
+            // Sanitize the video title to remove invalid characters from the file name
+            string sanitizedTitle = string.Join("_", video.Title.Split(Path.GetInvalidFileNameChars()));
+
+            // Get all available muxed streams
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+            var muxedStreams = streamManifest.GetMuxedStreams();
+            if (muxedStreams.Any())
+            {
+                var streamInfo = muxedStreams.First();
+                using var httpClient = new HttpClient();
+                var stream = await httpClient.GetStreamAsync(streamInfo.Url);
+                var datetime = DateTime.Now;
+
+                string outputFilePath = Path.Combine(outputDirectory, $"{sanitizedTitle}.{streamInfo.Container}");
+                using var outputStream = System.IO.File.Create(outputFilePath);
+                await stream.CopyToAsync(outputStream);
+
+                Console.WriteLine("Download completed!");
+                Console.WriteLine($"Video saved as: {outputFilePath}{datetime}");
+            }
+            else
+            {
+                Console.WriteLine($"No suitable video stream found for {video.Title}.");
+            }
         }
     }
 }
